80e893a45fc1b39dd1e10da97f712ad09cbd01cf
Juan Fumero <jjfumero@gmail.com>
2025-02-24 13:32:39 +0100

    minor change

--- src/main/java/tornadovm/benchmarks/MatrixMultiplication.java
+++ src/main/java/tornadovm/benchmarks/MatrixMultiplication.java
@@ -38,7 +38,6 @@ import uk.ac.manchester.tornado.api.GridScheduler;
 import uk.ac.manchester.tornado.api.TaskGraph;
 import uk.ac.manchester.tornado.api.TornadoExecutionPlan;
 import uk.ac.manchester.tornado.api.WorkerGrid;
-import uk.ac.manchester.tornado.api.WorkerGrid1D;
 import uk.ac.manchester.tornado.api.WorkerGrid2D;
 import uk.ac.manchester.tornado.api.annotations.Parallel;
 import uk.ac.manchester.tornado.api.enums.DataTransferMode;
